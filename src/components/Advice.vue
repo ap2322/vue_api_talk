98940b59daaac026ff6a71ffdbf7765bec73e2b1
Alice Post <alicep2322@gmail.com>
2020-06-12 17:11:48 -0500

    Add simple request for advice

--- src/components/Advice.vue
+++ src/components/Advice.vue
@@ -1,16 +1,25 @@
 <template>
     <div>
         <p> {{ info }} </p>
+        <button @click="getAdvice" class="btn btn-primary">Get Advice</button>
     </div>
 </template>
 
 <script>
+const axios = require('axios')
 
 export default {
   name: 'Advice',
   data () {
     return {
       info: 'placeholder'
+    }
+  },
+  methods: {
+    getAdvice: function () {
+      axios
+        .get('https://api.adviceslip.com/advice')
+        .then(response => (this.info = response))
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
+
+Vue.use(Axios)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
-
